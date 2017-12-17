d8e650cf51502f14d59bfa4a058954f8f780326e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-17 15:01:33 +0700

    PSR2 Coding standard

--- InnerDateBoundary.php
+++ InnerDateBoundary.php
@@ -1,105 +1,106 @@
 <?php
 
-/** 
+/**
  * @author Ammar Faizi <ammarfaizi2@gmail.com>
  * @license MIT
  */
 final class InnerDateBoundary
 {
-	/**
-	 * @var array
-	 */
-	private $bound = [];
+    /**
+     * @var array
+     */
+    private $bound = [];
 
-	/**
-	 * @var string
-	 */
-	private $delimeter;
+    /**
+     * @var string
+     */
+    private $delimeter;
 
-	/**
-	 * @var array
-	 */
-	private $result = [];
+    /**
+     * @var array
+     */
+    private $result = [];
 
-	/**
-	 * Constructor
-	 *
-	 * @param string ...$argv
-	 */
-	public function __construct(...$argv)
-	{
-		$this->bound = $argv;
-	}
+    /**
+     * Constructor
+     *
+     * @param string ...$argv
+     */
+    public function __construct(...$argv)
+    {
+        $this->bound = $argv;
+    }
 
-	private function explodeDate()
-	{
-		foreach ($this->bound as &$val) {
-			$val = explode($this->delimeter, $val);
-			if (sizeof($val) !== 2) {
-				throw new \Exception("Invalid boundary!", 1);
-			}
-			self::getDiffList($val);
-		}
-	}
+    private function explodeDate()
+    {
+        foreach ($this->bound as &$val) {
+            $val = explode($this->delimeter, $val);
+            if (sizeof($val) !== 2) {
+                throw new \Exception("Invalid boundary!", 1);
+            }
+            self::getDiffList($val);
+        }
+    }
 
-	private static function getDiffList(&$val)
-	{
-		$sv = $val;
-		$tmp = [];
-		if ($sv[0] > $sv[1]) {
-			$tmp[] = $sv[1];
-			while ($sv[1] !== $sv[0]) {
-				$sv[1] = $tmp[] = date("Y-m-d", strtotime($sv[1]."+ 1 day"));
-			}
-		} elseif ($sv[0] < $sv[1]) {
-			$tmp[] = $sv[0];
-			while ($sv[1] !== $sv[0]) {
-				$sv[0] = $tmp[] = date("Y-m-d", strtotime($sv[0]."+ 1 day"));
-			}
-		}
-		$val = $tmp;
-	}
+    private static function getDiffList(&$val)
+    {
+        $sv = $val;
+        $tmp = [];
+        if ($sv[0] > $sv[1]) {
+            $tmp[] = $sv[1];
+            while ($sv[1] !== $sv[0]) {
+                $sv[1] = $tmp[] = date("Y-m-d", strtotime($sv[1]."+ 1 day"));
+            }
+        } elseif ($sv[0] < $sv[1]) {
+            $tmp[] = $sv[0];
+            while ($sv[1] !== $sv[0]) {
+                $sv[0] = $tmp[] = date("Y-m-d", strtotime($sv[0]."+ 1 day"));
+            }
+        }
+        $val = $tmp;
+    }
 
-	private function getUnique()
-	{
-		$saver = []; $completer = [];
-		foreach ($this->bound as $key => $val) {
-			foreach ($val as $subval) {
-				$completer[] = $saver[$key][] = $subval;
-			}
-		}
-		foreach ($completer as $val) {
-			$flag = true;
-			foreach ($saver as $subval) {
-				if (! in_array($val, $subval)) {
-					$flag = false;
-					break;
-				}
-			}
-			$flag and !in_array($val, $this->result) and $this->result[] = $val;
-		}
-		
-	}
+    private function getUnique()
+    {
+        $saver = [];
+        $completer = [];
+        foreach ($this->bound as $key => $val) {
+            foreach ($val as $subval) {
+                $completer[] = $saver[$key][] = $subval;
+            }
+        }
+        foreach ($completer as $val) {
+            $flag = true;
+            foreach ($saver as $subval) {
+                if (! in_array($val, $subval)) {
+                    $flag = false;
+                    break;
+                }
+            }
+            $flag and !in_array($val, $this->result) and $this->result[] = $val;
+        }
+        
+    }
 
-	public function getResult()
-	{
-		return $this->result;
-	}
+    public function getResult()
+    {
+        return $this->result;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function execute()
-	{
-		$this->explodeDate();
-		$this->getUnique();
-	}
+    /**
+     * @return bool
+     */
+    public function execute()
+    {
+        $this->explodeDate();
+        $this->getUnique();
+    }
 
-	/**
-	 * @param string $delimeter
-	 */
-	public function setDelimeter($delimeter)
-	{
-		$this->delimeter = $delimeter;
-	}
+    /**
+     * @param string $delimeter
+     */
+    public function setDelimeter($delimeter)
+    {
+        $this->delimeter = $delimeter;
+    }
 }
--- main.php
+++ main.php
@@ -2,10 +2,10 @@
 
 require __DIR__ . "/InnerDateBoundary.php";
 
-$st = new InnerDateBoundary (
-	"2017-12-17 to 2017-12-20",
-	"2017-12-18 to 2017-12-22",
-	"2017-12-17 to 2017-12-21"
+$st = new InnerDateBoundary(
+    "2017-12-17 to 2017-12-20",
+    "2017-12-18 to 2017-12-22",
+    "2017-12-17 to 2017-12-21"
 );
 $st->setDelimeter(" to ");
 $st->execute();
